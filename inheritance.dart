76b311b8abc3eaad209e230743b4da0da30393c7
Pu-reun-deul Kim <prd106401@naver.com>
2025-03-27 19:17:49 +0900

    forth commit

--- inheritance.dart
+++ inheritance.dart
@@ -16,18 +16,33 @@ class Idol {
 }
 
 class BoyGroup extends Idol {
-  BoyGroup(String name, int membersCount): super(name, membersCount);
+  final String home;
+
+  // 일부 변수는 부모 생성자를 호출하고 일부는 자식 필드 초기화(home)
+  BoyGroup(String name, int membersCount, this.home): super(name, membersCount);
 
   // 새로운 기능
   void sayMale() {
     print('저는 남자 아이돌입니다.');
   }
+
+  // 자식 필드 사용
+  void sayHome() {
+    print('저희 팀은 ${this.home}에 소속되어 있습니다.');
+  }
+
+  // 부모 필드와 자식 필드 동시에 사용(이름이 중복되면 'super.' 사용으로 부모 필드 호출)
+  void sayFull() {
+    print('저는 ${name}입니다. 저의 팀은 총 ${membersCount}명으로 구성되어 있으며 ${this.home}에 소속되어 있습니다.');
+  }
 }
 
 void main() {
-  BoyGroup bts = BoyGroup('BTS', 7);
+  BoyGroup bts = BoyGroup('BTS', 7, 'HIBE');
 
   bts.sayName();
   bts.sayMembersCount();
   bts.sayMale();
+  bts.sayHome();
+  bts.sayFull();
 }
